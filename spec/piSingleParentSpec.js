355d37f85e0e9dc988f8c88adbd5631db17b2b00
Benwei Zhu <benw1988@yeah.net>
2013-09-28 16:54:45 +0800

    [BenweiZhu] add jasmine unit test to project

---
+++ spec/piSingleParentSpec.js
@@ -0,0 +1,5 @@
+describe("For condition qq and qq", function() {
+    it("it should return true when input is 12 12 12 12", function() {
+        expect(condition_qq_qq(12,12,12,12)).toBe(true);
+    });
+});
