bd4ecc1dc5f42caf3e5f93058f4dea3bb7c6ca43
Benwei Zhu <benw1988@yeah.net>
2013-09-28 17:01:11 +0800

    [BenweiZhu] add unit test to condition qq_qq and pq_qq

--- spec/piSingleParentSpec.js
+++ spec/piSingleParentSpec.js
@@ -2,4 +2,26 @@ describe("For condition qq and qq", function() {
     it("it should return true when input is 12 12 12 12", function() {
         expect(condition_qq_qq(12,12,12,12)).toBe(true);
     });
+
+    it("it should return false when input is 12 13 12 12", function() {
+        expect(condition_qq_qq(12,13,12,12)).toBe(false);
+    });
+});
+
+describe("For condition pq and qq", function() {
+    it("it should return true when input is 11 12 12 12", function() {
+        expect(condition_pq_qq(11,12,12,12)).toBe(true);
+    });
+
+    it("it should return true when input is 12 13 12 12", function() {
+        expect(condition_pq_qq(12,13,12,12)).toBe(true);
+    });
+
+    it("it should return false when input is 12 12 12 12", function() {
+        expect(condition_pq_qq(12,12,12,12)).toBe(false);
+    });
+
+    it("it should return false when input is 12 13 12 13", function() {
+        expect(condition_pq_qq(12,13,12,13)).toBe(false);
+    });
 });
