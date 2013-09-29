0d43da5a189b7aee28e07c0f8243542848894cf3
Benwei Zhu <benw1988@yeah.net>
2013-09-29 22:22:28 +0800

    [BenweiZhu] add condition pq pq pq, pq pq qq, pq pq qr

--- js/calculator/piParents.js
+++ js/calculator/piParents.js
@@ -194,6 +194,22 @@ function condition_pq_pr_or_ps_qr(C1, C2, M1, M2, AF1, AF2) {
         (((M1 == C1 || M1 == C2) && (M2 != C1 && M2 != C2)) || ((M2 == C1 || M2 == C2) && (M1 != C1 && M1 != C2))) &&
         (((AF1 == C1 || AF1 == C2) && (AF2 != C1 && AF2 != C2)) || ((AF2 == C1 || AF2 == C2) && (AF1 != C1 && AF1 != C2)));
 }
+function condition_pq_pq_pq(C1, C2, M1, M2, AF1, AF2) {
+    return C1 != C2 && M1 != M2 && AF1 != AF2 &&
+        (C1 == AF1 || C1 == AF2) && (C2 == AF1 || C2 == AF2) &&
+        (C1 == M1 || C1 == M2) && (C2 == M1 || C2 == M2) &&
+        (M1 == AF1 || M1 == AF2) && (M2 == AF1 || M2 == AF2);
+}
+function condition_pq_pq_qq(C1, C2, M1, M2, AF1, AF2) {
+    return C1 != C2 && M1 != M2 && AF1 == AF2 &&
+        (C1 == M1 || C1 == M2) && (C2 == M1 || C2 == M2) &&
+        (AF1 == C1 || AF1 == C2);
+}
+function condition_pq_pq_qr(C1, C2, M1, M2, AF1, AF2) {
+    return C1 != C2 && M1 != M2 && AF1 != AF2 &&
+        (C1 == M1 || C1 == M2) && (C2 == M1 || C2 == M2) &&
+        (((AF1 == C1 || AF1 == C2) && (AF2 != C1 && AF2 != C2)) || ((AF2 == C1 || AF2 == C2) && (AF1 != C1 && AF1 != C2)));
+}
 function calculatePi(rowID) {
     var locus = piParentsFindObj("locus_" + (rowID), document).value;
     var AF1 = piParentsFindObj("AF1_" + (rowID), document).value;
@@ -214,49 +230,71 @@ function calculatePi(rowID) {
     if (condition_qq_pq_qq(C1, C2, M1, M2, AF1, AF2)) {
         pi = 1 / Number(C1value);
     }
+
     if (condition_qq_qq_qq(C1, C2, M1, M2, AF1, AF2)) {
         pi = 1 / Number(C1value);
     }
+
     if (condition_qq_pq_qr(C1, C2, M1, M2, AF1, AF2)) {
         pi = 1 / (2 * Number(C1value));
     }
+
     if (condition_qq_qq_qr(C1, C2, M1, M2, AF1, AF2)) {
         pi = 1 / (2 * Number(C1value));
     }
+
     if (condition_pq_pp_qq(C1, C2, M1, M2, AF1, AF2)) {
         pi = 1 / Number(AF1value);
     }
+
     if (condition_pq_pr_qq(C1, C2, M1, M2, AF1, AF2)) {
         pi = 1 / Number(AF1value);
     }
+
     if (condition_pq_pp_pq(C1, C2, M1, M2, AF1, AF2)) {
         if (C1 == M1) {
-            pi = 1 / Number(C2);
+            pi = 1 / Number(C2value);
         } else {
-            pi = 1 / Number(C1);
+            pi = 1 / Number(C1value);
         }
     }
+
     if (condition_pq_pr_or_ps_pq(C1, C2, M1, M2, AF1, AF2)) {
         if (M1 == C1 || M1 == C2) {
-            pi = 1 / Number(M1);
+            pi = 1 / Number(M1value);
         } else {
-            pi = 1 / Number(M2);
+            pi = 1 / Number(M2value);
         }
     }
+
     if (condition_pq_pp_qr(C1, C2, M1, M2, AF1, AF2)) {
         if (C1 == M1) {
-            pi = 1 / Number(C2);
+            pi = 1 / Number(C2value);
         } else {
-            pi = 1 / Number(C1);
+            pi = 1 / Number(C1value);
         }
     }
+
     if (condition_pq_pr_or_ps_qr(C1, C2, M1, M2, AF1, AF2)) {
         if (C1 == M1) {
-            pi = 1 / Number(C2);
+            pi = 1 / Number(C2value);
         } else {
-            pi = 1 / Number(C1);
+            pi = 1 / Number(C1value);
         }
     }
+
+    if (condition_pq_pq_pq(C1, C2, M1, M2, AF1, AF2)) {
+        pi = 1 / (Number(C1value) + Number(C2value));
+    }
+
+    if (condition_pq_pq_qq(C1, C2, M1, M2, AF1, AF2)) {
+        pi = 1 / (Number(C1value) + Number(C2value));
+    }
+
+    if (condition_pq_pq_qr(C1, C2, M1, M2, AF1, AF2)) {
+        pi = 1 / 2 * (Number(C1value) + Number(C2value));
+    }
+
     if (C1 != C2) {
         if (AF1 == AF2 && (AF1 == C1 || AF1 == C2)) {
             if (M1 != M2 && (M1 == C1 || M1 == C2) && (M2 == C1 || M2 == C2)) {
