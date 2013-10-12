192c9f8609bf412b1a1edeb81d894a24d1a9598a
Benwei Zhu <benw1988@yeah.net>
2013-10-12 23:23:56 +0800

    [BenweiZhu] create calculatorHelper.js

--- js/calculator/AGCU211/piParents.js
+++ js/calculator/AGCU211/piParents.js
@@ -1,17 +1,3 @@
-function piParentsFindObj(theObj, theDoc) {
-    var p, i, foundObj;
-    if (!theDoc) theDoc = document;
-    if ((p = theObj.indexOf("?")) > 0 && parent.frames.length) {
-        theDoc = parent.frames[theObj.substring(p + 1)].document;
-        theObj = theObj.substring(0, p);
-    }
-    if (!(foundObj = theDoc[theObj]) && theDoc.all) foundObj = theDoc.all[theObj];
-    for (i = 0; !foundObj && i < theDoc.forms.length; i++) foundObj = theDoc.forms[i][theObj];
-    for (i = 0; !foundObj && theDoc.layers && i < theDoc.layers.length; i++) foundObj = piParentsFindObj(theObj, theDoc.layers[i].document);
-    if (!foundObj && document.getElementById) foundObj = document.getElementById(theObj);
-    return foundObj;
-}
-
 function loadTableFromCookie() {
     var linesCount = getCookie("piParentsLinesCount");
     for (var i = 1; i <= Number(linesCount); i++) {
